0940738e39530673ecb925eb16035442a6fb759d
Levi Appenfelder <leviapp@Levis-MacBook-Pro.local>
2022-07-05 14:42:11 -0600

    getting unit data

--- src/components/UnitList.js
+++ src/components/UnitList.js
@@ -1,16 +1,20 @@
 import React, {useState, useEffect} from 'react';
-
+import Unit from './Unit'
 function UnitList(props) {
-  const [unitList, setUnitList] = useState(null)
+    const [loading, setLoading] = useState(true)
+    const [unitListDetails, setUnitListDetails] = useState(null)
   useEffect(() => {
-    setUnitList(props.list)
-
+    setLoading(false)
+    setUnitListDetails(props.unitData)
   }, [])
   return (
-    <div className="Main">
-        {loading ? <h1>Loading...</h1> : <h1>{pageInfo["per_page"]}</h1>}
-        {loading ? <h1>Loading...</h1> : <h1>{area1["total_count"]}</h1>}
-        {loading ? <h1>Loading...</h1> : <h1>{areaMore["total_count"]}</h1>}
+    <div className="unitlist">
+        {loading ? <h1>Loading...</h1> : <h1>{unitListDetails["total_count"]}</h1>}
+        {loading ? <h1>Loading...</h1> : 
+        <section>{unitListDetails["data"].map(val => <Unit key={val.id} unitData={val}/>)
+        
+        }</section>}
+        {/* {loading ? <h1>Loading...</h1> : <h1>{areaMore["total_count"]}</h1>} */}
 
     </div>
   );
